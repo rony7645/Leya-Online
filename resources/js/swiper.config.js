74630bbfb6b3fd30d170f6b651f83cd1e9dd8b2c
rony7645 <nirony731@gmail.com>
2025-05-25 18:34:59 +0600

    #

--- resources/js/swiper.config.js
+++ resources/js/swiper.config.js
@@ -1,108 +1,72 @@
 import Swiper from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
-import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/css/pagination";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
-const swiper = new Swiper(".banner-swiper", {
-  modules: [Navigation, Autoplay],
+// Default configuration for Swiper
+const defaultSwiperConfig = {
+  modules: [Navigation,Pagination, Autoplay],
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 1,
-    },
-    992: {
-      slidesPerView: 1,
-    },
-  },
-  spaceBetween: 20,
-  speed: 500,
-  loop: true,
-});
-
-const productOverviewSwiper = new Swiper(".product-overview-swiper", {
-  modules: [Navigation, Autoplay],
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "bullets",
   },
   autoplay: {
     delay: 8000,
     disableOnInteraction: false,
   },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 1,
-    },
-    992: {
-      slidesPerView: 1,
-    },
-  },
   spaceBetween: 20,
   speed: 500,
   loop: true,
+  allowTouchMove: true,
+  freeMode: true,
+};
+
+// Function to initialize a Swiper instance
+const initializeSwiper = (selector, customConfig = {}) => {
+  return new Swiper(selector, {
+    ...defaultSwiperConfig,
+    ...customConfig,
+  });
+};
+
+// Initialize Swiper instances
+initializeSwiper(".banner-swiper", {
+  modules: [Navigation, Pagination, Autoplay],
+  autoplay: { delay: 5000 },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 1 },
+    992: { slidesPerView: 1 },
+  },
 });
 
-const categoriesSwiper = new Swiper(".categories-swiper", {
-  modules: [Navigation, Autoplay],
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  autoplay: {
-    delay: 8000,
-    disableOnInteraction: false,
-  },
+initializeSwiper(".product-overview-swiper", {
   breakpoints: {
-    0: {
-      slidesPerView: 3,
-    },
-    768: {
-      slidesPerView: 4,
-    },
-    992: {
-      slidesPerView: 5,
-    },
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 1 },
+    992: { slidesPerView: 1 },
   },
-  spaceBetween: 20,
-  speed: 500,
-  loop: true,
 });
 
-const producstCarousel = new Swiper(".products-swiper", {
-  modules: [Navigation, Autoplay],
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  autoplay: {
-    delay: 8000,
-    disableOnInteraction: false,
-  },
+initializeSwiper(".categories-swiper", {
   breakpoints: {
-    0: {
-      slidesPerView: 2.5,
-    },
-    768: {
-      slidesPerView: 3.5,
-    },
-    992: {
-      slidesPerView: 6,
-    },
+    0: { slidesPerView: 3 },
+    768: { slidesPerView: 4 },
+    992: { slidesPerView: 5 },
   },
+});
+
+initializeSwiper(".products-swiper", {
   spaceBetween: 5,
-  speed: 500,
-  loop: true,
+  breakpoints: {
+    0: { slidesPerView: 2.5 },
+    768: { slidesPerView: 3.5 },
+    992: { slidesPerView: 6 },
+  },
 });
