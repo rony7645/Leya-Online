ed1592a993907956effbb2154d4188ae8b7c5898
rony7645 <nirony731@gmail.com>
2025-05-28 22:17:01 +0600

    #

--- public/layout/header.js
+++ public/layout/header.js
@@ -3,3 +3,13 @@ fetch("../pages/header.html")
   .then((data) => {
     document.getElementById("header").innerHTML = data;
   });
+
+
+        document.querySelectorAll('.dropdown-item').forEach(item => {
+            item.addEventListener('click', function (e) {
+                e.preventDefault();
+                const btn = document.querySelector('.category-btn span');
+                btn.textContent = this.textContent;
+            });
+        });
+    
