8a179135aa154530fb40a09c4fd40c202bd8b2f6
rony7645 <nirony731@gmail.com>
2025-06-07 21:15:43 +0600

    #

--- resources/js/swiper.config.js
+++ resources/js/swiper.config.js
@@ -73,6 +73,33 @@ const configMap = {
       992: { slidesPerView: 1 },
     },
   },
+  "shop-bestsellers-swiper": {
+    spaceBetween: 10,
+    breakpoints: {
+      0: { slidesPerView: 2 },
+      768: { slidesPerView: 2 },
+      992: { slidesPerView: 3 },
+      1200: { slidesPerView: 4 },
+    },
+  },
+  "shop-discount-swiper": {
+    spaceBetween: 10,
+    breakpoints: {
+      0: { slidesPerView: 2 },
+      768: { slidesPerView: 2 },
+      992: { slidesPerView: 3 },
+      1200: { slidesPerView: 4 },
+    },
+  },
+  "shop-2product-swiper": {
+    spaceBetween: 10,
+    breakpoints: {
+      0: { slidesPerView: 2 },
+      768: { slidesPerView: 2 },
+      992: { slidesPerView: 2 },
+    },
+  },
+  
 };
 
 // Initialize Swiper for all sliders
